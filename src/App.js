c822b6cdb1ed8347fbd7dd9062511ef2946711f9
TurtleGirlz <user.email>
2022-06-25 10:27:22 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+import React, {Component} from 'react';
+import './bootstrap.min.css'
+import './centerdiv.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="container">
+        <div className="row justify-content-center align-items-center">
+            <div className="col-md-6">
+                <img src={require("./images/big-diamond.png")} alt="" />
+                <div>
+                    <h2>Cryft Cards</h2>
+                </div>
+                <div>
+                    <h3>Welcome to the Moon</h3>
+                </div>
+                <div>
+                    <Button onClick={() => authenticate() }>Authenticate</Button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
